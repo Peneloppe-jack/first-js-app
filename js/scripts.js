f32d5384914ba4b0d074df27e08f38806e782afe
Peneloppe-jack <94473375+Peneloppe-jack@users.noreply.github.com>
2022-01-11 13:18:15 +0100

    Building app
    I created a list of 7 pokemons : as objects
    Then added 3 keys to each :
    names - as strings
    Height - simple number
    types - 2 keys within an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,48 @@
 alert('Hello world');
 
-let favoriteFood = 'Pizza';
+let PokemonList= [
+  {
+    name:'chamander',
+    height: 0.6,
+    type:  ['fire fang' , 'body slam']
+  },
 
-console.log(favoriteFood);
-document.write(favoriteFood);
+  {
+    name: 'pidgey',
+    height: 0.3,
+    type: ['takle' , 'aerial ace']
+  },
+
+  {
+    name: 'rattata',
+    height: 0.3,
+    type:['sucker punch' , 'blizzard']
+  },
+
+  {
+    name:'fearow',
+    height:1.2,
+    type: ['growl' , 'curse']
+  },
+
+  {
+    name: 'pikachu',
+    height: 0.4,
+    type: ['thundershock' , 'thunder']
+  },
+
+  {
+    name:'vulpix',
+    height: 0.6,
+    type: ['roar' , 'flame burst']
+  },
+
+  {
+    name:'meowth',
+    height: 0.4,
+    type: ['bite' , 'nasty plot']
+  }
+
+]
+
+console.log(PokemonList)
