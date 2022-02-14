5b5a173e884a268eada8515fc8b8d80cae80accb
Daniela T <94473375+Peneloppe-jack@users.noreply.github.com>
2022-02-14 14:38:02 +0100

    Loading data from an external source / Pokemon API
    If changes applied :
     - the ApiUrl inclueded in the Pokemon repository, allows me to collect the dynamic list of pokemon.
    - I added some conditions to be verify by the add function concerning the type of object
    - The Asnyc functions  loadList and loadDetails using the fetch method to get pokemonlist and their details  from the "ApiUrl have been created and return the Json function
    - The showDetails function has been then updated such as the for each loop that access the IFFE to implement all these chnages.

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,25 @@ alert('Hello world');
 //creating a pokemon respository in which I included the pokemon´s list
 
 let PokemonRepository = (function () {
-let PokemonList= [
-  {
-    name:'chamander',
-    height: 0.6,
-    type:  ['fire fang' , 'body slam']
-  },
-  {
-    name: 'pidgey',
-    height: 0.3,
-    type: ['takle' , 'aerial ace']
-  },
-  {
-    name: 'rattata',
-    height: 0.3,
-    type:['sucker punch' , 'blizzard']
-  },
-  {
-    name:'fearow',
-    height:1.2,
-    type: ['growl' , 'curse']
-  },
-  {
-    name: 'pikachu',
-    height: 0.4,
-    type: ['thundershock' , 'thunder']
-  },
-  {
-    name:'vulpix',
-    height: 0.6,
-    type: ['roar' , 'flame burst']
-  },
-  {
-    name:'meowth',
-    height: 0.4,
-    type: ['bite' , 'nasty plot']
-  }
-];
+let PokemonList= [];
+let ApiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+// The pokenomonlist is an empty Array linked to the Pokemon API 
+// This allows me to switch from the static list that I made to A complete list of Pokemon 
 
   function add (pokemon) {
-    PokemonList.push(pokemon);
-}
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ){
+      PokemonList.push(pokemon);
+    } else {
+       console.log("pokemon is not correct");
+      }
+    }
+// the code above creates a function, condition, loop, to verify the type of object inserted into code 
   function getAll() {
-    return PokemonList;
+  return PokemonList;
 }
 
   function addListItem (pokemon) {
@@ -56,38 +32,72 @@ let PokemonList= [
     ListPokemon.appendChild(Button);
     PokemonList.appendChild(ListPokemon);
     Button.addEventListener('click', function () {
-    showDetails (pokemon);
+      showDetails (pokemon);
 });
+}
+// the code above creates a button, event listener that is called,logged at every click 
 
+  async function loadList() {
+  try {
+    const response = await fetch(ApiUrl);
+    const json = await response.json();
+    json.results.forEach(function (item) {
+      let pokemon = {
+        name: item.name,
+        detailsUrl: item.url
+      };
+      add(pokemon);
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
-  function showDetails (pokemon) {
-    console.log (pokemon.name);
+// this is an Asnyc function using the fetch method to get pokemonlist from the "ApiUrl"
+// the result = the "response" = a "promise" = the Json function passed as a parameter of the fetch ()?
+
+  async function loadDetails(item) {
+  let url = item.detailsUrl;
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
+    // let details to item =
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  } catch (e) {
+    console.error(e);
+  }
 }
-  // creating a show details function + console log + calling the showDetails function
-  // calling the showdetails function by the addEventListener to apply it
+
+// this is an Asnyc function using the fetch method to get pokemon details from the "ApiUrl"
+// the result = the "response" = a "promise" = the Json function passed as a parameter passed as a parameter of the fetch ()?
+
+function showDetails(item) {
+   PokemonRepository.loadDetails(item).then(function () {
+     console.log(item);
+   });
+ }
+
+ // this is a show details function in which the loadDetails function is passed ass a parameter
 
   return {
     add: add,
     getAll: getAll,
     addListItem : addListItem,
-    showDetails : showDetails,
+    loadList : loadList,
+    loadDetails : loadDetails,
+    showDetails : showDetails
   };
 })();
 
-//* The code above is wrapped into an IIFE which protects your code and executes it automatically thanks to the ()
-PokemonRepository.add({
-        name:'wigglytuff',
-        height: 1,
-        type: ['sing' , 'nasty cute-charm']});
-console.log(PokemonRepository.getAll());
+// The code above is wrapped into an IIFE which protects your code and executes it automatically thanks to the ()
+// while creating a function inside an IIFE remember to call it again in the return part of the function right before closing the function with extra ()
+// this goes for all the previously created fucntion in the IFFE
 
-//* The code above (thanks to the function add) adds a single Item into the  repository saved in the IFFE
-//* The getAll function returns an array in the console
-//After adding a pokemon to the main list  with the function add or push
-//I wrote a function PokemonRepository.getAll().forEach  to access the IFFE + the new item and writes them all on the page
-PokemonRepository.getAll().forEach (function(pokemon) {
-  PokemonRepository.addListItem (pokemon);
+PokemonRepository.loadList().then(function () {
+  PokemonRepository.getAll().forEach(function (pokemon) {
+    PokemonRepository.addListItem(pokemon);
+  });
 });
 
-// while creating a function inside an IIFE remember to call again in the return part of the function 
-// this goes for the addListItem function + show details function
+// forEach loop that access the IFFE to load list, details, add Pokemon 
